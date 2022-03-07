3a5efca8a0bcc09c51a5a8b52ac327c1c879828b
Joshua <jwaiswa7@gmail.com>
2022-03-07 07:28:02 +0300

    Update the search controller to call the search service

--- app/controllers/users/searches_controller.rb
+++ app/controllers/users/searches_controller.rb
@@ -3,5 +3,10 @@
 module Users
   class SearchesController < ApplicationController
     def new; end
+
+    def create
+        SearchService.new(question: params[:question], user: params[:user_id]).call 
+        head :ok
+    end
   end
 end
